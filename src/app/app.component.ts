6b412a719d5dcd2edca26c07693fedeecc455639
DimaMakarenko <makarenko.d.viri@gmail.com>
2022-10-08 21:26:46 +0300

    feat: API error handler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { products as data } from './data/products';
+import { Observable, tap } from 'rxjs';
+import { ProductService } from './services/products.service';
 import { IProduct } from './models/product';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular app'
+  // products: IProduct[] = []
+  loading = false
+  products$: Observable<IProduct[]>
 
-  products: IProduct[] = data
+  constructor(private productService: ProductService) {
+  }
+
+  ngOnInit(): void {
+    this.loading = true
+    this.products$ = this.productService.getProducts().pipe(
+      tap(() => this.loading = false)
+    )
+
+    // this.productService.getProducts().subscribe((products) => {
+    //   this.products = products
+    //   this.loading = false
+    // })
+
+  }
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,12 +1,38 @@
-import { Injectable } from '@angular/core';
-
+import { ErrorService } from './error.service';
+import { IProduct } from './../models/product';
+import { Injectable, } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http'
+import { catchError, delay, Observable, retry, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class ProductService {
-  constructor(){
-    
+  constructor(
+    private http: HttpClient,
+    private errorService: ErrorService
+  ) {
+
+  }
+
+  getProducts(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>('https://fakestoreapi.com/products', {
+      params: new HttpParams({
+        // fromString: 'limit=5'
+        fromObject: {
+          limit: 5
+        }
+      })
+    }).pipe(
+      delay(200),
+      retry(2),
+      catchError(this.errorHandler.bind(this))
+    )
+  }
+
+  private errorHandler(error: HttpErrorResponse) {
+    this.errorService.handle(error.message)
+    return throwError(() => error.message)
   }
 }
